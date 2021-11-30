005509aa24d5f6a01e2388b666c8594c5131d1f6
Luis <Luis@LAPTOP-0EFHT9GS>
2021-11-29 20:25:31 -0500

    Improve footer styles

--- src/common/component/Footer/index.jsx
+++ src/common/component/Footer/index.jsx
@@ -4,29 +4,28 @@ import styles from './footerPage.module.css';
 
 function Footer() {
   const { options, setCurrentElection } = useContext(ChatContext)
+  const { optionsStyles, footer } = styles
   return (
-    <div className={styles.footer}>
-      <div className={styles.escritorio}>
-        {
-          options?.map((option, index) => {
-            return (
-            <button 
-              className={styles.boton} 
-              key={index} 
-              onClick={()=> {
-                if(option.handler){
-                  option.handler(option)
-                }
-                setCurrentElection({
-                  ...option
-                })
-              }}>
-              {option.message}
-            </button>
-            )
-          })
-        }
-      </div>
+    <div className={footer}>
+      {
+        options?.map((option, index) => {
+          return (
+          <button 
+            className={optionsStyles} 
+            key={index} 
+            onClick={()=> {
+              if(option.handler){
+                option.handler(option)
+              }
+              setCurrentElection({
+                ...option
+              })
+            }}>
+            {option.message}
+          </button>
+          )
+        })
+      }
     </div>
   );
 }
